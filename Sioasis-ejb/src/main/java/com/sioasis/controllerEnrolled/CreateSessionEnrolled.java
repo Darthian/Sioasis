a7be7eb07c9b9492c3da477854573112772179bd
darthian <darthian@Darthian-PC.Sarasty>
2013-03-13 18:27:18 -0500

    sessionBean Insertar Managed

---
+++ Sioasis-ejb/src/main/java/com/sioasis/controllerEnrolled/CreateSessionEnrolled.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sioasis.controllerEnrolled;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author darthian
+ */
+@Stateless
+@LocalBean
+public class CreateSessionEnrolled {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+
+    
+}
